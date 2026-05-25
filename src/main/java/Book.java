e5b0cb1913347f47c3e7c1e65163d9f252827dc4
Boraz10 <austinborazio@gmail.com>
2026-05-25 16:12:04 +1000

    test

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,17 @@
 public class Book {
 
+    private String ID;
+    private String title;
+    private int quantity;
+    private double price;
+    private String type;
+
+    public Book(String ID, String title, int quantity, double price, String type) {
+        this.ID = ID;
+        this.title = title;
+        this.quantity = quantity;
+        this.price = price;
+        this.type = type;
+    }
+
 }
--- src/main/java/BookRepository.java
+++ src/main/java/BookRepository.java
@@ -12,4 +12,5 @@ public class BookRepository {
             System.out.println(books);
         }
     }
+
 }
